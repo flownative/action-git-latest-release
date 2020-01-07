9738cfe4215535cdbb090ad70604ff50b4f79383
Robert Lemke <robert@flownative.com>
2020-01-07 10:32:31 +0100

    Fetch more history

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -x
 
-git fetch --depth=1 origin +refs/tags/*:refs/tags/*
+git fetch --depth=100 origin +refs/tags/*:refs/tags/*
 echo ::set-output name=tag::$(git describe --tags --match='v*' --abbrev=0)
