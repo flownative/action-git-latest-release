4ca4a921f93a4f9254bc98f588917db6bed19748
Karsten Dambekalns <karsten@dambekalns.de>
2022-04-19 08:21:34 +0200

    Add /github/workspace as safe Git directory
    see https://github.blog/2022-04-12-git-security-vulnerability-announced/

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 
+git config --global --add safe.directory /github/workspace
 git fetch --depth=100 origin +refs/tags/*:refs/tags/*
 echo ::set-output name=tag::$(git describe --tags --match='v*' --abbrev=0)
 echo ::set-output name=version::$(git describe --tags --match='v*' --abbrev=0 | grep -oE '[0-9.]+' | head -n 1)
