edbaaa746577df4f8e0af9775fdbaeba4732e4fb
Karsten Dambekalns <karsten@dambekalns.de>
2023-02-22 19:10:05 +0100

    Use GITHUB_OUTPUT environment file
    See https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -x
 
 git config --global --add safe.directory ${GITHUB_WORKSPACE}
 git fetch --depth=100 origin +refs/tags/*:refs/tags/*
-echo ::set-output name=tag::$(git describe --tags --match='v*' --abbrev=0)
-echo ::set-output name=version::$(git describe --tags --match='v*' --abbrev=0 | grep -oE '[0-9.]+' | head -n 1)
+echo "tag=$(git describe --tags --match='v*' --abbrev=0)" >> $GITHUB_OUTPUT
+echo "version=$(git describe --tags --match='v*' --abbrev=0 | grep -oE '[0-9.]+' | head -n 1)" >> $GITHUB_OUTPUT
